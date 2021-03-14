d458684041be578f953cd6d419cdc6ec567e2e74
jamiro <jamirograntsaan@gmail.com>
2021-03-14 15:56:45 +0100

    fixed faulty redirection after login. also, added login

--- modules/website.py
+++ modules/website.py
@@ -1,18 +1,32 @@
-from flask import Flask, render_template, safe_join, send_from_directory, request, redirect
+from flask import Flask, render_template, safe_join, send_from_directory, request, redirect, url_for
+from flask_login import LoginManager, UserMixin, login_required, current_user, login_user, logout_user, fresh_login_required
 from flask_sockets import Sockets
 from time import sleep
 from gevent import pywsgi
 from geventwebsocket.handler import WebSocketHandler
+# for debugging
+from werkzeug.serving import run_with_reloader
+from werkzeug.debug import DebuggedApplication
 
 import os, json
-
+# source for  the goodgood: https://gist.github.com/danielfennelly/9a7e9b71c0c38cd124d0862fd93ce217
 
 from components.logger import Logger
+DEBUG = True
+# For flask_login
+class User(UserMixin):
+    def __init__(self):
+        self.auth = False
 
+    """
+    @property
+    def is_authenticated(self):
+        return self.auth
+    """
 class Website:
     def __init__(self, Networking=None, Watcher=None, Database=None):
         self.dependencies = {"tier": "user", "dependencies":["Networking", "Watcher", "Database"]}
-        self.capabilities = ["ui", "input", "blocking"]
+        self.capabilities = ["ui", "input", "blocking", "question"]
         #self.timing = {"unit": "minutes", "count":2}
         self.networking = Networking
         self.watcher = Watcher
@@ -28,6 +42,8 @@ class Website:
         # detect other ui elements
         pass
 
+    def question(self, question):
+        pass
     def query(self, query, connectionID = 666):
         """connectionID is the id of who is asking(starts at 0, database is 999)
            the query is a dict containing the following keys:
@@ -60,7 +76,7 @@ class Website:
             if qtype == "template":
                 """ get template data for presets """
                 self.logger("template request")
-                self.logger(query)
+                #self.logger(query)
                 preset = qdata["preset"] # e.g. weather
                 files = qdata["filenames"] # e.g. weather.html
                 elementsize = qdata["size"] # either small or big(for expanded card)
@@ -75,7 +91,7 @@ class Website:
                     finres = {}
                     for filename in files:
                         extension = filename.split(".")[-1]
-                        self.logger(extension, "info", "blue")
+                        #self.logger(extension, "info", "blue")
                         if extension in ["js", "html", "json"]:
                             finpath = os.path.join(tmppath, filename)
                             with open(finpath) as f:
@@ -83,7 +99,7 @@ class Website:
                             if extension == "json":
                                 result = json.loads(result)
                             finres[filename] = result
-                    self.logger(finres, "alert")
+                    #self.logger(finres, "alert")
                     response = self.networking.messagebuilder("web", "template", finres, metadata)
                 except Exception as e1:
                     self.logger(f"Error: {str(e1)}")
@@ -95,22 +111,53 @@ class Website:
 
         # TODO: Read out the query
         # TODO: Use it to write out the response
-        self.logger(response, "debug", "blue")
+        #self.logger(response, "debug", "blue")
         return response
 
     def startrun(self):
         staticfolder = os.path.join(self.basefolder, "static")
         tmpfolder = os.path.join(self.basefolder, "templates")
         self.app = Flask(__name__, static_folder=staticfolder, template_folder=tmpfolder)
+        self.app.secret_key = "haaaaaaaaaaaaai"
+        self.app.config['TRAP_BAD_REQUEST_ERRORS'] = True
         self.socket = Sockets(self.app)
+        self.login_manager = LoginManager()
+        self.login_manager.init_app(self.app)
+        self.login_manager.login_view = "login"
+
         ssl_enabled = False
 
         self.logger("website is running")
 
         @self.app.route("/")
-        def hello():
+        @fresh_login_required
+        def index():
             return render_template("index.html")
 
+
+        @self.app.route('/login', methods=['GET', 'POST'])
+        def login():
+            if request.method == 'GET':
+                return '''
+                   <form action='login' method='POST'>
+                    <input type='hidden' name='text' id='hidden'  value'1'></input>
+
+                    <input type='password' name='pw' id='pw' placeholder='password'></input>
+                    <input type='submit' name='submit'></input>
+                   </form>
+                   '''
+
+            storedpasswd = self.db.query(["web-ui", "password"], "credentials")["resource"]
+            if request.form['pw'] == storedpasswd:
+                user = User()
+                user.auth= True
+                user.id = 1
+                login_user(user)
+                return redirect(url_for('index'))
+
+
+            return 'Bad login'
+    
         @self.app.before_request
         def before_request():
             if not request.is_secure and ssl_enabled:
@@ -132,8 +179,18 @@ class Website:
                 return 404
 
 
+        @self.login_manager.user_loader
+        def user_loader(id):
+            user = User()
+            user.id = self.db.query("name", "personalia")["resource"]
+            return user
+
         # check if tls/ssl is available
         ssl_enabled = self.db.query(["ssl_enabled"], "system")["resource"]
+
+        # check if debug requested
+        if DEBUG:
+            self.app = DebuggedApplication(self.app)
         if ssl_enabled:
             certchain_location = self.db.query(["certchain"], "system")["resource"]
             keyfile_location = self.db.query(["keyfile"], "system")["resource"]
