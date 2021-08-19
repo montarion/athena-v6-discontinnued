bb0e5a4db27b29957b5400fd0d81ebbe60f019f4
jamiro <jamirograntsaan@gmail.com>
2021-08-19 22:02:26 +0200

    cleanup

--- modules/spotify.py
+++ modules/spotify.py
@@ -33,44 +33,6 @@ class Spotify:
         # TODO: Use it to write out the response
         return response
 
-    def oldgetauthorized(self):
-        # let user login
-        baseurl = "https://accounts.spotify.com/authorize"
-        scopes = ["user-read-currently-playing", "user-modify-playback-state", "user-library-modify"]
-        client_id = self.db.query(["spotify", "client_id"], "credentials")["resource"]
-        params= {
-                "client_id":client_id, # TODO: guide the user to get their own
-                "response_type": "code",
-                #"redirect_uri": "https://github.com/montarion/athena",
-                "scope": ' '.join(scopes)
-                }
-        #r = requests.get(baseurl, params=params)
-        auth_code = self.oauth.getauthorized(baseurl, params)
-        return auth_code
-        #loginurl = r.url
-        #self.logger(loginurl)
-        
-        # Figure out how you want to do setup stuff. (just use the desktop client for now!)
-        # write an OAUTH support class(nonspecific, so it works with all oauth stuff)
-
-
-    def authorize(self):
-        # STUB, get this from the getauthorized() method
-        token = self.db.query(["spotify", "auth_token"], "credentials")
-        return {"token": token, "success": True}
-
-    def oldgetaccess(self):
-        # check if accesstoken
-        
-        baseurl = "https://accounts.spotify.com/api/token"
-        
-        # if not accesstoken
-        auth_code = self.getauthorized()
-        #auth_code = "AQCyFSpjHrniBnyQBHwng-2gDUMVKNYhKm3jKTW6pQ63HBQ9sJrrExIZ3AS-mkoXmw4SGYn2Wc3WB_oPFIkZXVU7sXIQvNeKOMILrBIqen4lpTti8__sB9qSbaSiM_z-EN9_TPnNe0u1fScm5hex9YlCIcsCZqQVs8KSoZsHqaMdwjRopqfi27Y1OTQmDcZqUumoKhX-ENVaa3GpFhpVyTN2HNnq_vwFWmCbH0Nx-j7GNTA29zKQwDcPuMmammnPodnJ6FNW7cGDEHpv1t3mQ5gtl9y8"
-        client_id = self.db.query(["spotify", "client_id"], "credentials")["resource"]
-        client_secret = self.db.query(["spotify", "client_secret"], "credentials")["resource"]
-        return self.oauth.getaccesstoken(baseurl, auth_code, client_id, client_secret)
-
     def getplaying(self):
         ac = self.oauth.getaccesstoken()
         baseurl = "https://api.spotify.com/v1/me/player/currently-playing"
